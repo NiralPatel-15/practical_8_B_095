209b8f9614770448985faf1389a26110a61e91d2
Niral Patel <niralpatel1502@gmail.com>
2026-03-23 22:11:36 +0530

    Added Practical-8 set B Program

---
+++ script.js
@@ -0,0 +1,25 @@
+function generatePattern() {
+  const type = document.getElementById("patternType").value;
+  const symbol = document.getElementById("symbol").value || "*";
+  const rows = parseInt(document.getElementById("rows").value);
+
+  let result = "";
+
+
+  if (type === "left") {
+    for (let i = 1; i <= rows; i++) {
+      result += (symbol + " ").repeat(i) + "\n";
+    }
+  }
+
+ 
+  else if (type === "inverted") {
+    for (let i = rows; i >= 1; i--) {
+      let spaces = " ".repeat(rows - i);
+      let stars = (symbol + " ").repeat(i);
+      result += spaces + stars + "\n";
+    }
+  }
+
+  document.getElementById("output").innerText = result;
+}
